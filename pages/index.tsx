fe9ee497334b7fa17813281de39cf5968b95a49f
David Ernst <git@dsernst.com>
2022-03-20 19:38:51 -0700

    Replace placeholder links w/ link to source code

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,32 +24,14 @@ const Home: NextPage = () => {
         </p>
 
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
           <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
+            href="https://github.com/dsernst/DhammaGong"
+            target="_blank"
             className="card"
+            rel="noreferrer"
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <h2>Code &rarr;</h2>
+            <p>View or Contribute to this app&apos;s source code.</p>
           </a>
         </div>
       </main>
