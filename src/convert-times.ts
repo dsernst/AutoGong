9deaa9b174e8baf143fb57d76c8897bdf013c648
David Ernst <git@dsernst.com>
2022-03-21 01:11:29 -0700

    Fix convertTimes() for first 9 minute after the hour

--- src/convert-times.ts
+++ src/convert-times.ts
@@ -5,17 +5,18 @@
  */
 export const convertTimes = (times: number): string => {
   const hour = Math.floor(times / 100)
-  const minute = times % 100
+  const minute = String(times % 100).padStart(2, '0')
   const ampm = hour >= 12 ? 'pm' : 'am'
   let hour12 = hour > 12 ? hour % 12 : hour
   if (hour12 === 0) hour12 = 12 // for 12 am
-  return `${hour12}:${String(minute).padEnd(2, '0')} ${ampm}`
+  return `${hour12}:${minute} ${ampm}`
 }
 
 // Unit tests
 const testCases = {
   27: '12:27 am',
   800: '8:00 am',
+  108: '1:08 am',
   1250: '12:50 pm',
   1545: '3:45 pm',
 }
