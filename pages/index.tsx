0cb9da6b415b6d2a572352b9b742dcb483bf7f59
David Ernst <git@dsernst.com>
2022-03-20 18:19:05 -0700

    Switch to styled-jsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div className="container">
       <Head>
         <title>Dhamma Gong</title>
         <meta
@@ -14,28 +13,27 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to Dhamma Gong!</h1>
+      <main>
+        <h1>Welcome to Dhamma Gong!</h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
+        <p className="description">
+          Get started by editing <code>pages/index.tsx</code>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+        <div className="grid">
+          <a href="https://nextjs.org/docs" className="card">
             <h2>Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
+          <a href="https://nextjs.org/learn" className="card">
             <h2>Learn &rarr;</h2>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
+            className="card"
           >
             <h2>Examples &rarr;</h2>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
@@ -43,7 +41,7 @@ const Home: NextPage = () => {
 
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+            className="card"
           >
             <h2>Deploy &rarr;</h2>
             <p>
@@ -52,6 +50,104 @@ const Home: NextPage = () => {
           </a>
         </div>
       </main>
+
+      <style jsx>{`
+        .container {
+          padding: 0 2rem;
+        }
+
+        main {
+          min-height: 100vh;
+          padding: 4rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        h1 a {
+          color: #0070f3;
+          text-decoration: none;
+        }
+
+        h1 a:hover,
+        h1 a:focus,
+        h1 a:active {
+          text-decoration: underline;
+        }
+
+        h1 {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 3rem;
+        }
+
+        h1,
+        .description {
+          text-align: center;
+        }
+
+        .description {
+          margin: 4rem 0;
+          line-height: 1.5;
+          font-size: 1.5rem;
+        }
+
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+        }
+
+        .grid {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-wrap: wrap;
+          max-width: 800px;
+        }
+
+        @media (max-width: 600px) {
+          .grid {
+            width: 100%;
+            flex-direction: column;
+          }
+        }
+
+        .card {
+          margin: 1rem;
+          padding: 1.5rem;
+          text-align: left;
+          color: inherit;
+          text-decoration: none;
+          border: 1px solid #eaeaea;
+          border-radius: 10px;
+          transition: color 0.15s ease, border-color 0.15s ease;
+          max-width: 300px;
+        }
+
+        .card:hover,
+        .card:focus,
+        .card:active {
+          color: #0070f3;
+          border-color: #0070f3;
+        }
+
+        .card h2 {
+          margin: 0 0 1rem 0;
+          font-size: 1.5rem;
+        }
+
+        .card p {
+          margin: 0;
+          font-size: 1.25rem;
+          line-height: 1.5;
+        }
+      `}</style>
     </div>
   )
 }
